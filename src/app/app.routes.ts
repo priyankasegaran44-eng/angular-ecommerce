a3d75260ecce3aefdb8d4d1e7c84547339e5a5e1
Priyanka <priyankasegaran44@gmail.com>
2026-07-02 15:23:03 +0530

    Added header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { ProductDetails } from './pages/product-details/product-details';
+import { Cart } from './pages/cart/cart';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'products', component: Products },
+  { path: 'product/:id', component: ProductDetails },
+  { path: 'cart', component: Cart },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register }
+];
